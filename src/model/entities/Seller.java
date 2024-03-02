a860418999d7f41558135099a3cac2f01d4b68e4
Joyce Mendes Miranda <joycemendes.jm79@gmail.com>
2024-03-02 15:48:06 -0300

    insert

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,17 +1,18 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.sql.Date;
-import java.util.Objects;
+import java.util.Date;
 
 public class Seller implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	private String email;
 	private Date birthDate;
 	private Double baseSalary;
+	
 	private Department department;
 	
 	public Seller() {
@@ -76,7 +77,10 @@ public class Seller implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -88,7 +92,12 @@ public class Seller implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Seller other = (Seller) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
@@ -96,8 +105,4 @@ public class Seller implements Serializable {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
-
-	
-	
-	
 }
