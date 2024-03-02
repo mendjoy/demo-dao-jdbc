0fbc6102df7cc1d025738ff9155969798699ce53
Joyce Mendes Miranda <joycemendes.jm79@gmail.com>
2024-03-02 17:01:17 -0300

    delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TESTE 1: seller findById ===");
@@ -37,11 +39,19 @@ public class Program {
 	    sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
-		 System.out.println("\n === TESTE 5: seller update ===");
-		 seller = sellerDao.findById(1);
-		 seller.setName("Marta Waine");
-		 sellerDao.update(seller);
-		 System.out.println("Update completed");
+		System.out.println("\n === TESTE 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
+		System.out.println("\n === TESTE 6: seller delete ===");
+		System.out.print("Informe um ID: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete complete");
+		
+		sc.close();
 	}
 
 }
